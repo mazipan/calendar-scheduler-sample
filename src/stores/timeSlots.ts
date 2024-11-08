1c1ad6d97ffef878e56bae635bdfc2328d83263d
mazipan <mazipanneh@gmailcom>
2024-11-08 18:39:51 +0700

    FEAT: Use store for slot list

--- src/stores/timeSlots.ts
+++ src/stores/timeSlots.ts
@@ -3,81 +3,51 @@ import { defineStore } from 'pinia'
 import { nanoid } from 'nanoid'
 import dayjs from 'dayjs'
 
-export const useTimeslotStore = defineStore('timeSlot', () => {
+export const getNewSlotItem = () => {
   const dStart = new Date()
   dStart.setHours(7, 0, 0, 0)
   const instanceStart = dayjs(dStart)
 
+  const defaultSlots = {
+    id: nanoid(),
+    start: instanceStart,
+    end: instanceStart,
+  }
+
+  return defaultSlots
+}
+
+export const useTimeslotStore = defineStore('timeSlot', () => {
+  const defaultSlots = getNewSlotItem()
+
   const timeslots = ref({
     '1': {
       active: false,
-      slots: [
-        {
-          id: nanoid,
-          start: instanceStart,
-          end: instanceStart,
-        },
-      ],
+      slots: [defaultSlots],
     },
     '2': {
       active: false,
-      slots: [
-        {
-          id: nanoid,
-          start: instanceStart,
-          end: instanceStart,
-        },
-      ],
+      slots: [defaultSlots],
     },
     '3': {
       active: false,
-      slots: [
-        {
-          id: nanoid,
-          start: instanceStart,
-          end: instanceStart,
-        },
-      ],
+      slots: [defaultSlots],
     },
     '4': {
       active: false,
-      slots: [
-        {
-          id: nanoid,
-          start: instanceStart,
-          end: instanceStart,
-        },
-      ],
+      slots: [defaultSlots],
     },
     '5': {
       active: false,
-      slots: [
-        {
-          id: nanoid,
-          start: instanceStart,
-          end: instanceStart,
-        },
-      ],
+      slots: [defaultSlots],
     },
     '6': {
       active: false,
-      slots: [
-        {
-          id: nanoid,
-          start: instanceStart,
-          end: instanceStart,
-        },
-      ],
+      slots: [defaultSlots],
     },
     '7': {
       active: false,
-      slots: [
-        {
-          id: nanoid,
-          start: instanceStart,
-          end: instanceStart,
-        },
-      ],
+      slots: [defaultSlots],
     },
   })
 
