af76350cc4e18f6e7a9c3ceec4d9c93286d82959
mazipan <mazipanneh@gmail.com>
2024-11-10 23:06:09 +0700

    FEAT: Very basic calendar view

--- src/libs/date.ts
+++ src/libs/date.ts
@@ -1,5 +1,10 @@
 import type { TimeConfig } from '@/types/TimeSlot'
 import dayjs from 'dayjs'
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
+
+dayjs.extend(isSameOrAfter)
+dayjs.extend(isSameOrBefore)
 
 export const getDayjs = () => {
   return dayjs()
@@ -7,6 +12,21 @@ export const getDayjs = () => {
 
 export const formatDate = (date: string, format: string) => {
   return dayjs(date).format(format)
+}
+
+export const timeToDayJs = (timeStr: string, dateJs: dayjs.Dayjs) => {
+  const newCompared = `${dateJs.format('YYYY-MM-DD')} ${timeStr.replace('.', ' ')}`
+  return dayjs(newCompared, 'YYYY-MM-DD h:mm a')
+}
+
+export const isInTimeRange = (time: dayjs.Dayjs, start: string, end: string) => {
+  const startDayjs = timeToDayJs(start, time)
+  const endDayjs = timeToDayJs(end, time)
+
+  const isSameOrAfterStart = time.isSameOrAfter(startDayjs.format('YYYY-MM-DD HH:mm:ss'), 'minutes')
+  const isSameOrBeforeEnd = time.isSameOrBefore(endDayjs.format('YYYY-MM-DD HH:mm:ss'), 'minutes')
+
+  return isSameOrAfterStart && isSameOrBeforeEnd
 }
 
 export const generateTimes = () => {
